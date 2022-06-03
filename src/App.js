9834e3854e42a2a49e4c48382ca466d010009a95
Arkadiusz Zygan <a.zygan@gmail.com>
2022-06-03 15:57:43 +0200

    Add portfolio component

--- src/App.js
+++ src/App.js
@@ -8,16 +8,14 @@ import Contact from "./components/Contact/Contact.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 
 import { AppContext } from "./context/AppContext";
-import ThemeButton from "./components/ThemeButton/ThemeButton.jsx";
 
 const App = () => {
-  const { theme, setTheme } = useContext(AppContext);
-  console.log(theme);
+  const { theme } = useContext(AppContext);
+
   return (
     <>
       <div className={`${theme}`}>
         <div className="background text">
-          {/* <ThemeButton /> */}
           <Header />
           <Nav />
           <About />
